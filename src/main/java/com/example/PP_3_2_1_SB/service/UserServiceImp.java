3f1626fd9a203ea20446659a226be41ed4a0ab44
aiminkai <m.a.azarka@gmail.com>
2022-12-09 22:58:48 +0300

    Data JPA

--- src/main/java/com/example/PP_3_2_1_SB/service/UserServiceImp.java
+++ src/main/java/com/example/PP_3_2_1_SB/service/UserServiceImp.java
@@ -1,6 +1,6 @@
 package com.example.PP_3_2_1_SB.service;
 
-import com.example.PP_3_2_1_SB.DAO.UserDAO;
+import com.example.PP_3_2_1_SB.DAO.UserRepository;
 import com.example.PP_3_2_1_SB.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,44 +8,44 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Transactional
 @Service
 public class UserServiceImp implements UserService {
 
-    private final UserDAO userDao;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserServiceImp(UserDAO userDao) {
-        this.userDao = userDao;
+    public UserServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
     public void create(User user) {
-        userDao.create(user);
+        userRepository.save(user);
     }
 
     @Override
     public List<User> getAllUsers() {
-
-        return userDao.getAllUsers();
+        return userRepository.findAll();
     }
 
     @Override
     public void deleteUser(int id) {
-        userDao.deleteUser(id);
+        userRepository.deleteById(id);
     }
 
     @Override
     public void updateUser(User user) {
-        userDao.updateUser(user);
+        userRepository.save(user);
     }
 
     @Override
     public User getUserById(int id) {
-
-        return userDao.getUserById(id);
+        Optional<User> foundUser = userRepository.findById(id);
+        return foundUser.orElse(null);
     }
 
 }
